8752e5a00338ba4ba729033a91cf06795e40a34c
xTrayambak <xtrayambak@disroot.org>
2024-09-09 13:55:57 +0530

    (fix) parseInt: follow the ECMA262 spec as closely as possible
    We're now fairly compliant to how ECMA262's 2023 edition specifies
    parseInt should behave! :^)

--- src/bali/stdlib/builtins/parse_int.nim
+++ src/bali/stdlib/builtins/parse_int.nim
@@ -6,7 +6,7 @@ import std/[strutils, math, options, logging]
 import mirage/ir/generator
 import mirage/runtime/prelude
 import bali/runtime/normalize
-import bali/internal/sugar
+import bali/internal/[sugar, trim_string]
 import pretty
 
 proc parseIntGenerateStdIr*(vm: PulsarInterpreter, generator: IRGenerator) =
@@ -17,35 +17,43 @@ proc parseIntGenerateStdIr*(vm: PulsarInterpreter, generator: IRGenerator) =
   generator.newModule("parseInt")
   vm.registerBuiltin("BALI_PARSEINT",
     proc(op: Operation) =
-      if vm.registers.callArgs.len < 1 or vm.registers.callArgs[0].kind != String:
+      if vm.registers.callArgs.len < 1:
         vm.registers.retVal = some floating NaN
         return
 
       let 
-        value = &vm.registers.callArgs[0].getStr()
-        radix = if vm.registers.callArgs.len > 1:
+        inputString = vm.registers.callArgs[0] # 1. Let inputString be ? ToString(string).
+        value = vm.trimString(inputString, TrimMode.Left) # 2. Let S be ! TrimString(inputString, start).
+
+        # FIXME: should we interpret the rest as according to the spec or should we leave it to the Nim standard library? It seems to work as intended...
+        radix = if vm.registers.callArgs.len > 1: # 8. If R ≠ 0, then
           vm.registers.callArgs[1].getInt()
         else:
-          if value.startsWith("0x"):
-            some(16)
-          else:
-            some(10)
+          # We don't remove the first two chars from the beginning of the string as `parseInt` in std/strutils does it itself when the radix is set to 16.
+          if value.startsWith("0x"): # 10. a. If the length of S is at least 2 and the first two code units of S are either "0x" or "0X", then 
+            some(16)                 # ii. Set R to 16.
+          else:                      # 9. Else,
+            some(10)                 # a. Set R to 10.
       
       try:
         vm.registers.retVal = some(
-          if radix == some(2):
+          case &radix
+          of 2:
             integer parseBinInt(value)
-          elif radix == some(8):
+          of 8:
             integer parseOctInt(value)
-          elif radix == some(10):
+          of 10:
             integer parseInt(value)
-          elif radix == some(16):
+          of 16:
             integer parseHexInt(value)
           else:
-            floating NaN
+            if unlikely(&radix < 2 or &radix > 36):
+              floating NaN
+            else:
+              floating NaN
         )
       except ValueError as exc:
-        warn "builtins.parse_int(" & $value & "): " & exc.msg & " (radix " & $radix & ')'
+        warn "builtins.parse_int(" & $value & "): " & exc.msg & " (radix=" & $radix & ')'
         vm.registers.retVal = some floating NaN
   )
   generator.call("BALI_PARSEINT")
