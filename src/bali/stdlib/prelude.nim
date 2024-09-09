35c6bde03f516c54993719885ea5ec2d74828be3
xTrayambak <xtrayambak@disroot.org>
2024-09-09 13:55:42 +0530

    (add) stdlib: export test262 and errors

--- src/bali/stdlib/prelude.nim
+++ src/bali/stdlib/prelude.nim
@@ -1,4 +1,4 @@
-import ./[console, math, uri]
-import ./builtins/[parse_int]
+import ./[console, math, uri, errors]
+import ./builtins/[parse_int, test262]
 
-export console, math, uri, parse_int
+export console, math, uri, parse_int, errors, test262
