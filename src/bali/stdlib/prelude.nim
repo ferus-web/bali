e0b9f2e52e081eba3336e47b45d9e8afab997ec5
xTrayambak <xtrayambak@disroot.org>
2024-09-12 19:30:44 +0530

    (add) base64: finalize base64 module
    Though not part of the ECMAScript spec, we'll still need this in Ferus.
    So, why not implement it here itself and be done with it? ;)

--- src/bali/stdlib/prelude.nim
+++ src/bali/stdlib/prelude.nim
@@ -1,4 +1,4 @@
 import ./[console, math, uri, errors]
-import ./builtins/[parse_int, test262]
+import ./builtins/[base64, parse_int, test262]
 
-export console, math, uri, parse_int, errors, test262
+export console, math, uri, parse_int, errors, test262, base64
