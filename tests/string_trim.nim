3be68fc1c6cb1896a5d9a5e6c3cd4bc2cf15402a
xTrayambak <xtrayambak@disroot.org>
2024-09-13 10:58:50 +0530

    (add) tests: TrimString() test
    It currently fails for TrimMode.Right, gotta fix up our implementation.

--- tests/string_trim.nim
+++ tests/string_trim.nim
@@ -1,5 +1,12 @@
-import std/[strutils]
+import std/[strutils, unittest]
 import bali/internal/trim_string
 
-let x = "   32".internalTrim(strutils.Whitespace, TrimMode.Left)
-echo x
+
+suite "TrimString( string, where )":
+  test "trim leading whitespace":
+    check "   32".internalTrim(strutils.Whitespace, TrimMode.Left) == "32"
+    check "           :^)".internalTrim(strutils.Whitespace, TrimMode.Left) == ":^)"
+
+  test "trim ending whitespace":
+    check "32   ".internalTrim(strutils.Whitespace, TrimMode.Right) == "32"
+    check ":^)                  ".internalTrim(strutils.Whitespace, TrimMode.Right) == ":^)"
