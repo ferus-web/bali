ebbef387c7bf92dddae4b46a85312860206ef758
xTrayambak <xtrayambak@disroot.org>
2024-09-25 18:58:52 +0530

    (tests) update URL parser test

--- tests/data/uri_parse.js
+++ tests/data/uri_parse.js
@@ -1,2 +1,3 @@
-const url = new URL("ewr:/e!112wrnc")
-console.log(url)
+var url = new URL("https://google.com:65535") // this works.
+var url = new URL("https:google.com") // this will throw a TypeError as the URL is malformed
+var url = new URL("https://google.com:65536") // this will throw a TypeError as the port is outside of the valid range
