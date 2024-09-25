29c08158cebf6797cd420fca563adf7ab2b3224f
xTrayambak <xtrayambak@disroot.org>
2024-09-25 18:59:06 +0530

    (tests) update base64 encode-decode test

--- tests/data/b64_encode_decode.js
+++ tests/data/b64_encode_decode.js
@@ -1,3 +1,6 @@
 // Used for the benchmark.
 let x = btoa("Both NodeJS's base64 module and Bali's builtin base64 module use simdutf. How much do they differ in terms of speed?")
 let y = atob("QmFsaSBpcyBuZWF0LCBJIGd1ZXNzLiAoSWdub3JlIHRoZSBmYWN0IHRoYXQgYSBCYWxpIGRldmVsb3BlciB3cm90ZSB0aGlzIHRlc3Qp")
+
+console.log(x)
+console.log(y)
