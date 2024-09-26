99921a9db3701793cda6201c3ab038ede63beb36
xTrayambak <xtrayambak@disroot.org>
2024-09-26 19:42:45 +0530

    (add) internal: `parseNumber`

---
+++ src/bali/internal/parse_number.nim
@@ -0,0 +1,14 @@
+import std/[strutils, logging, options]
+import bali/internal/sugar
+
+proc parseNumberText*(text: string): Option[float] {.inline.} =
+  debug "parseNumberText: " & text
+
+  try:
+    return float(parseInt(text)).some()
+  except ValueError as exc:
+    debug "parseNumberText: " & exc.msg
+    try:
+      return float(parseFloat(text)).some()
+    except ValueError as exc:
+      debug "parseNumberText: malformed data provided: " & text & " (" & exc.msg & ')'
