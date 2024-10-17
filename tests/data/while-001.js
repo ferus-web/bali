5299182856b49683a26284b357e34d7b69922c38
xTrayambak <xtrayambak@disroot.org>
2024-10-17 12:30:10 +0530

    (tests) add tests for while loops

--- tests/data/while-001.js
+++ tests/data/while-001.js
@@ -2,6 +2,6 @@
  * We need to get the parser to parse these and the runtime to generate correct code for 'em.
 */
 
-while (true) {
+while (true == true) {
 	console.log("Hello, bali!")
 }
---
+++ tests/data/while-mutation-001.js
@@ -0,0 +1,8 @@
+// While loop that mutates its state
+
+var i = 0
+
+while (i < 32) {
+	i++
+	console.log(i)
+}
