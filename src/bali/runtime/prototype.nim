06cd62527c849f63d25b183e6b88f109902662d7
xTrayambak <xtrayambak@disroot.org>
2024-10-27 10:34:01 +0530

    (add) add groundwork for prototypes

---
+++ src/bali/runtime/prototype.nim
@@ -0,0 +1,9 @@
+import std/[tables]
+import bali/runtime/[atom_obj_variant, types]
+
+type
+  Prototype* = object
+    fields*: Table[string, AtomOrFunction[NativeFunction]]
+
+func initPrototype*(fields: Table[string, AtomOrFunction]): Prototype {.inline.} =
+  Prototype(fields: fields)
