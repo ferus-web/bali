9d23c8ee7fc0c547b4550c2c73ef0c4f19fdb6bf
xTrayambak <xtrayambak@disroot.org>
2024-11-03 12:07:58 +0530

    (tests) update prototypes test

--- tests/runtime/prototypes_001.nim
+++ tests/runtime/prototypes_001.nim
@@ -1,15 +1,19 @@
-import std/[options]
+import std/[options, tables, logging]
 import bali/grammar/prelude
 import bali/runtime/prelude
 import bali/runtime/abstract/coercion
 import mirage/atom
-import pretty
+import pretty, colored_logger
 
 let parser = newParser("""
-EpicClass.die()
+console.log(EpicClass.myName)
 """) # I've grown tired of manually writing the AST :(
 
 let program = parser.parse()
+print program
+
+addHandler newColoredLogger()
+setLogFilter(lvlAll)
 
 type
   EpicClass* = object
@@ -20,6 +24,7 @@ runtime.registerType(
   prototype = EpicClass,
   name = "EpicClass"
 )
+runtime.setProperty(EpicClass, "myName", str "Deine Mutter")
 runtime.defineFn(
   EpicClass,
   "die",
