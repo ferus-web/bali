e15b55573ec47987f63dff41d33fc2d074ba69ce
xTrayambak <xtrayambak@disroot.org>
2024-11-03 12:06:47 +0530

    (rm) runtime: remove stale prototypes code

--- src/bali/runtime/prototype.nim
+++
@@ -1,8 +0,0 @@
-import std/[tables]
-import bali/runtime/[atom_obj_variant, types]
-
-type Prototype* = object
-  fields*: Table[string, AtomOrFunction[NativeFunction]]
-
-func initPrototype*(fields: Table[string, AtomOrFunction]): Prototype {.inline.} =
-  Prototype(fields: fields)
