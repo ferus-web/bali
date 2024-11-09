1b8d23a80446dff9021de498d98fe0cee33af7ef
xTrayambak <xtrayambak@disroot.org>
2024-11-09 12:18:34 +0530

    (add) deps: add `results`

--- bali.nimble
+++ bali.nimble
@@ -22,3 +22,5 @@ requires "https://github.com/ferus-web/sanchar >= 2.0.0"
 requires "jsony >= 1.1.5"
 requires "crunchy >= 0.1.11"
 requires "climate >= 1.1.1"
+
+requires "results >= 0.5.0"
--- src/bali/internal/sugar.nim
+++ src/bali/internal/sugar.nim
@@ -1,6 +1,7 @@
 ## Some syntactical sugar for making code less clunky.
 
 import std/options
+import results
 
 {.push checks: off, inline.}
 func `*`*[T](opt: Option[T]): bool =
@@ -8,10 +9,22 @@ func `*`*[T](opt: Option[T]): bool =
 
 func `!`*[T](opt: Option[T]): bool =
   opt.isNone
+
+func `*`*[T, E](opt: Result[T, E]): bool =
+  opt.isOk
+
+func `!`*[T, E](opt: Result[T, E]): bool =
+  opt.isErr
 {.pop.}
 
 func `&`*[T](opt: Option[T]): T {.inline.} =
   opt.unsafeGet()
+
+func `&`*[T, E](opt: Result[T, E]): T {.inline.} =
+  opt.get()
+
+func `@`*[T, E](opt: Result[T, E]): E {.inline.} =
+  opt.error()
 
 func unpack*[T](opt: Option[T], x: var T): bool {.inline.} =
   if *opt:
