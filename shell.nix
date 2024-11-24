8da4aaeac63ddf90a6c17637ef7f19ced3c6ccfc
xTrayambak <xtrayambak@disroot.org>
2024-11-24 15:45:25 +0530

    (add) nix: add `gmp` as dependency

--- shell.nix
+++ shell.nix
@@ -4,11 +4,13 @@ mkShell {
   nativeBuildInputs = [
     pkg-config
     simdutf
+    gmp
     nodejs_22
     quickjs
   ];
 
   LD_LIBRARY_PATH = lib.makeLibraryPath [
     simdutf
+    gmp.dev
   ];
 }
