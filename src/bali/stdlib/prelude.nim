33348300bc1cd158bd9412bda9d95034a4907613
xTrayambak <xtrayambak@disroot.org>
2024-12-14 13:38:19 +0530

    (add) stdlib: add string prototype functions
    Also, `console` is a standalone type now.

--- src/bali/stdlib/prelude.nim
+++ src/bali/stdlib/prelude.nim
@@ -1,5 +1,6 @@
 import ./[console, math, uri, errors, errors_ir, errors_common, json, constants, date]
 import ./builtins/[base64, parse_int, test262, encode_uri]
+import ./types/[std_string]
 
 export console, math, uri, parse_int, errors, test262, base64, json, constants, date,
-       encode_uri, errors_ir, errors_common
+       encode_uri, errors_ir, errors_common, std_string
