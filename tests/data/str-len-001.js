407508a7a94d02d0f092b82f841580a88debe7d8
xTrayambak <xtrayambak@disroot.org>
2025-01-19 18:23:41 +0530

    (xxx) modify test

--- tests/data/str-len-001.js
+++ tests/data/str-len-001.js
@@ -1,2 +1,5 @@
 let x = "Hello world!"
-console.log(x.length)
+assert.sameValue(x.length, 12) // That's 12 codepoints in total.
+
+let y = "Können wir über die Schönheit der Wörter wie „Äpfel“, „Bäume“ und „Überraschung“ sprechen? Es ist großartig, wie die deutschen Umlaute ä, ö und ü unsere Sprache bereichern!"
+assert.sameValue(y.length, 90)
