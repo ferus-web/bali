9414fff556916c3efc47d5449db7f06c52e889e2
Trayambak Rai <xtrayambak@disroot.org>
2025-06-12 11:03:30 +0530

    fix: use bytecode VM for now
    The JIT isn't stable enough yet. It can be enabled via `--use-jit`

--- src/balde.nim
+++ src/balde.nim
@@ -64,7 +64,7 @@ proc allocRuntime*(ctx: Input, file: string, ast: AST, repl: bool = false): Runt
         loopAllocationEliminator: not ctx.enabled("disable-loop-allocation-elim"),
         aggressivelyFreeRetvals: ctx.enabled("aggressively-free-retvals"),
         deadCodeElimination: not ctx.enabled("disable-dead-code-elim"),
-        jitCompiler: not ctx.enabled("no-jit", "Z") and not repl,
+        jitCompiler: ctx.enabled("use-jit", "Z") and not repl,
       ),
     ),
   )
@@ -108,7 +108,7 @@ proc allocRuntime*(ctx: Input, file: string): Runtime =
         loopAllocationEliminator: not ctx.enabled("disable-loop-allocation-elim"),
         aggressivelyFreeRetvals: not ctx.enabled("aggressively-free-retvals"),
         deadCodeElimination: not ctx.enabled("disable-dead-code-elim"),
-        jitCompiler: not ctx.enabled("no-jit", "Z"),
+        jitCompiler: ctx.enabled("use-jit", "Z"),
       ),
     ),
     predefinedBytecode = readFile(file),
