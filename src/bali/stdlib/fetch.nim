fda2e5601afa77532d01a70515867ec2bff68127
xTrayambak <xtrayambak@gmail.com>
2025-08-01 18:28:55 +0530

    xxx: remove dead code

--- src/bali/stdlib/fetch.nim
+++
@@ -1,3 +0,0 @@
-## HTTP/fetch API
-## This uses ferus-sanchar by default, but you can change it by providing a compatible fetching delegate (like how Ferus will to shift the work to another process)
-import std/logging
