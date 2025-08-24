4c38441999c24431bfea0a85293546cb5a2c1464
xTrayambak <xtrayambak@gmail.com>
2025-08-24 12:16:11 +0530

    add: show an error if `pkg/bali` is imported

--- src/bali.nim
+++ src/bali.nim
@@ -1,1 +1,7 @@
-
+{.
+  error:
+    "Do not import `pkg/bali`, import the specific components you require instead.\n" &
+    "* If you wish to interpret JavaScript in a quick-and-dirty way, import `pkg/bali/easy`.\n" &
+    "* If you wish to parse JavaScript, import `pkg/bali/grammar/prelude`.\n" &
+    "* If you wish to fully control the JavaScript runtime, import `pkg/bali/runtime/prelude`."
+.}
