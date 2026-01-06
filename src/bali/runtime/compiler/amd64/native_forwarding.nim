454c956d4bce92039846921bec024dbb400c696f
xTrayambak <xtrayambak@gmail.com>
2026-01-06 18:20:02 +0530

    add: jit/amd64: `getRawInt()` impl

--- src/bali/runtime/compiler/amd64/native_forwarding.nim
+++ src/bali/runtime/compiler/amd64/native_forwarding.nim
@@ -1,10 +1,15 @@
 ## This file contains some helper functions used by the AMD64 JIT.
 ## These are often called by JIT'd code segments to "forward" work.
+##
+## Copyright (C) 2025-2026 Trayambak Rai (xtrayambak@disroot.org)
 import pkg/bali/runtime/vm/atom
 import pkg/shakar
 
 proc getRawFloat*(atom: JSValue): float64 {.cdecl.} =
   &atom.getNumeric()
+
+proc getRawInt*(atom: JSValue): int {.cdecl.} =
+  &atom.getInt()
 
 proc copyRaw*(dest, source: pointer, size: uint) {.cdecl.} =
   copyMem(dest, source, size)
